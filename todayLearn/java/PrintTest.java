e247c2b67bd3a8d8def03b9385b131b3d94d85c6
KimJinoook <99188096+KimJinoook@users.noreply.github.com>
2022-02-12 17:44:09 +0900

    Add files via upload

---
+++ todayLearn/java/PrintTest.java
@@ -0,0 +1,22 @@
+public class PrintTest{
+    public static void main(String[] args){
+        
+        // 33page
+        System.out.println("Bye, C");
+        System.out.println();
+        System.out.println("Bye, C++");
+        System.out.println();
+        System.out.println("Hello, Java!");
+        System.out.println();
+        
+        // 34page
+        System.out.print("이름 : 홍길동 ");
+        System.out.print("나이 : 20");
+
+        //35page
+        System.out.println();
+        System.out.println("2+5=" + 2 + 5);
+        System.out.println("2+5=" + (2+5));
+
+    }
+} 
